e0add4841555cfbf4ee0933b541b1d457a125559
okohaya <okohaya@gmail.com>
2018-06-04 18:01:49 +0900

    Redirect to login page if not authenticated

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('{any}', function () {
+    return response()->file(public_path('index.html'));
+})->where('any', '.*')->middleware('auth');
